513f430e487d75695bb08febcd0ed96a45dff408
JiaYidi47 <jyd9447@qq.com>
2015-06-25 20:37:52 +0800

    update UI

--- WebOxcoder/WebOxcoder/coderHome.aspx.designer.cs
+++ WebOxcoder/WebOxcoder/coderHome.aspx.designer.cs
@@ -13,42 +13,6 @@ namespace WebOxcoder {
     public partial class coderHome {
         
         /// <summary>
-        /// Form1 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm Form1;
-        
-        /// <summary>
-        /// select1 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlSelect select1;
-        
-        /// <summary>
-        /// select2 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlSelect select2;
-        
-        /// <summary>
-        /// province 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlSelect province;
-        
-        /// <summary>
         /// Form2 控件。
         /// </summary>
         /// <remarks>
